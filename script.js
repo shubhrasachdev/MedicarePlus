e3c0d499e46ef638ea779c1dbaf614331e98fe9e
Shubhra Sachdev <shubhra99sachdev@gmail.com>
2021-06-21 05:12:07 +0530

    cards aligned on landing

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ $('.left .nav-link').click(function(){
     $('.left .active').removeClass('active highlight');
     $(this).addClass('active highlight');
 });
+
+$('.card a').click(function(){
+    console.log($(this).attr("tgt"));
+
+});
